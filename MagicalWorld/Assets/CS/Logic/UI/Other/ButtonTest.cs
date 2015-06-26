c330cbf937cdb664d3a8e2a16daaaa0b9e8e96bf
搻诺 <377598034@qq.com>
2015-06-26 18:49:41 +0800

    更改地图资源

---
+++ MagicalWorld/Assets/CS/Logic/UI/Other/ButtonTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+public class ButtonTest : MonoBehaviour
+{
+   void Start()
+    {
+       EventTrigger trigger = gameObject.AddComponent<EventTrigger>();
+       EventTrigger.Entry click =  new EventTrigger.Entry();
+       click.eventID = EventTriggerType.PointerClick;
+       click.callback = new ClickEvet();
+       trigger.delegates.Add(click);
+    }
+}
+public class ClickEvet:EventTrigger.TriggerEvent
+{
+
+}
