dc534fb928799bfc280958d773c24e21ca79ef44
unknown <Administrator@KANKE-SXH.ikanke.cn>
2015-09-02 17:40:58 +0800

    增加战斗逻辑

---
+++ MagicalWorld/Assets/CS/Logic/Data/ActorData.cs
@@ -0,0 +1,53 @@
+﻿/// <summary>
+/// 角色数据类
+/// </summary>
+public class ActorData:BaseData
+{
+    public string name;
+    public int level;
+    public int HP;
+    public int attack;
+    /// <summary>
+    /// 移动速度
+    /// </summary>
+    public int moveSpeed;
+    /// <summary>
+    /// 攻击速度
+    /// </summary>
+    public int attackSpeed;
+    public int maxHP;
+    public ActorData(string _name,int _level,int _hp,int _attack,int _moveSpeed,int _attackSpeed)
+    {
+        name = _name;
+        level = _level;
+        HP = _hp;
+        attack = _attack;
+        moveSpeed = _moveSpeed;
+        attackSpeed = _attackSpeed;
+    }
+    /// <summary>
+    /// 拷贝
+    /// </summary>
+    /// <param name="data"></param>
+    public void CopyTo(ActorData data)
+    {
+        data.cfg = cfg;
+        data.name = name;
+        data.level = level;
+        data.HP = HP;
+        data.attack = attack;
+        data.moveSpeed = moveSpeed;
+        data.attackSpeed = attackSpeed;
+
+
+    }
+   /// <summary>
+   /// 比较
+   /// </summary>
+   /// <param name="data"></param>
+   /// <returns></returns>
+    public bool CompareTo(ActorData data)
+    {
+        return false;
+    }
+}
---
+++ MagicalWorld/Assets/CS/Logic/Data/BaseData.cs
@@ -0,0 +1,10 @@
+﻿using CG3D.Config;
+
+/// <summary>
+/// 数据基类
+/// </summary>
+public class BaseData 
+{
+    public DataAdapter cfg { get; set; }
+
+}
