967396fe0e4b872f16ed7e1c22d0962709b95bc1
搻諾 <377598034@qq.com>
2015-09-04 23:46:19 +0800

    增加逻辑

---
+++ MagicalWorld/Assets/CS/Logic/Config/ShipCfg.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using CG3D.Config;
+/// <summary>
+/// 舰船数据
+/// </summary>
+public class ShipCfg : BaseCfg
+{
+    private static ShipCfg _instance;
+    public static ShipCfg GetInstance()
+    {
+        if(_instance == null)
+        {
+            _instance = new ShipCfg();
+        }
+        return _instance;
+    }
+}
